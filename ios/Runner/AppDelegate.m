81dfdf1fbe4d6b2c73121f550ccb2ef178233d40
srebrin <srebrinb@gmail.com>
2018-12-14 13:30:31 +0200

    add uni_links pack

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,6 +1,7 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-
+// Add this to support Universal Links (see below)
+#import <uni_links/UniLinksPlugin.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
@@ -10,4 +11,14 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+// Add this to support Universal Links
+// NOTE: Necessary, until Flutter supports `application:continueUserActivity:restorationHandler`
+// within the `FlutterPlugin` protocol.
+- (BOOL)application:(UIApplication *)application
+    continueUserActivity:(NSUserActivity *)userActivity
+      restorationHandler:(void (^)(NSArray *_Nullable))restorationHandler {
+  return [[UniLinksPlugin sharedInstance] application:application
+                                 continueUserActivity:userActivity
+                                   restorationHandler:restorationHandler];
+}
 @end
