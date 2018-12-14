f6974d30f88dce219a67ae8d4d6845c3c8ccefe9
srebrin <srebrinb@gmail.com>
2018-12-14 17:30:13 +0200

    add firebase message

--- lib/main.dart
+++ lib/main.dart
@@ -1,304 +1,241 @@
+// Copyright 2017 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:async';
-import 'dart:io';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:uni_links/uni_links.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
+final Map<String, Item> _items = <String, Item>{};
+Item _itemForMessage(Map<String, dynamic> message) {
+  final String itemId = message['data']['id'];
+  final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))
+    ..status = message['data']['status'];
+  return item;
 }
 
-enum UniLinksType { string, uri }
+class Item {
+  Item({this.itemId});
+  final String itemId;
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  String _latestLink = 'Unknown';
-  Uri _latestUri;
+  StreamController<Item> _controller = StreamController<Item>.broadcast();
+  Stream<Item> get onChanged => _controller.stream;
 
-  StreamSubscription _sub;
+  String _status;
+  String get status => _status;
+  set status(String value) {
+    _status = value;
+    _controller.add(this);
+  }
 
-  TabController _tabController;
-  UniLinksType _type = UniLinksType.string;
+  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+  Route<void> get route {
+    final String routeName = '/detail/$itemId';
+    return routes.putIfAbsent(
+      routeName,
+      () => MaterialPageRoute<void>(
+            settings: RouteSettings(name: routeName),
+            builder: (BuildContext context) => DetailPage(itemId),
+          ),
+    );
+  }
+}
 
-  final List<String> _cmds = getCmds();
-  final TextStyle _cmdStyle = const TextStyle(
-      fontFamily: 'Courier', fontSize: 12.0, fontWeight: FontWeight.w700);
-  final _scaffoldKey = new GlobalKey<ScaffoldState>();
+class DetailPage extends StatefulWidget {
+  DetailPage(this.itemId);
+  final String itemId;
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Item _item;
+  StreamSubscription<Item> _subscription;
 
   @override
-  initState() {
+  void initState() {
     super.initState();
-    _tabController = new TabController(vsync: this, length: 2);
-    _tabController.addListener(_handleTabChange);
-    initPlatformState();
-  }
-
-  @override
-  dispose() {
-    if (_sub != null) _sub.cancel();
-    _tabController.removeListener(_handleTabChange);
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  initPlatformState() async {
-    if (_type == UniLinksType.string) {
-      await initPlatformStateForStringUniLinks();
-    } else {
-      await initPlatformStateForUriUniLinks();
-    }
-  }
-
-  /// An implementation using a [String] link
-  initPlatformStateForStringUniLinks() async {
-    // Attach a listener to the links stream
-    _sub = getLinksStream().listen((String link) {
-      if (!mounted) return;
-      setState(() {
-        _latestLink = link ?? 'Unknown';
-        _latestUri = null;
-        try {
-          if (link != null) _latestUri = Uri.parse(link);
-        } on FormatException {}
-      });
-    }, onError: (err) {
-      if (!mounted) return;
-      setState(() {
-        _latestLink = 'Failed to get latest link: $err.';
-        _latestUri = null;
-      });
-    });
-
-    // Attach a second listener to the stream
-    getLinksStream().listen((String link) {
-      print('got link: $link');
-    }, onError: (err) {
-      print('got err: $err');
-    });
-
-    // Get the latest link
-    String initialLink;
-    Uri initialUri;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      initialLink = await getInitialLink();
-      print('initial link: $initialLink');
-      if (initialLink != null) initialUri = Uri.parse(initialLink);
-    } on PlatformException {
-      initialLink = 'Failed to get initial link.';
-      initialUri = null;
-    } on FormatException {
-      initialLink = 'Failed to parse the initial link as Uri.';
-      initialUri = null;
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _latestLink = initialLink;
-      _latestUri = initialUri;
-    });
-  }
-
-  /// An implementation using the [Uri] convenience helpers
-  initPlatformStateForUriUniLinks() async {
-    // Attach a listener to the Uri links stream
-    _sub = getUriLinksStream().listen((Uri uri) {
-      if (!mounted) return;
-      setState(() {
-        _latestUri = uri;
-        _latestLink = uri?.toString() ?? 'Unknown';
-      });
-    }, onError: (err) {
-      if (!mounted) return;
-      setState(() {
-        _latestUri = null;
-        _latestLink = 'Failed to get latest link: $err.';
-      });
-    });
-
-    // Attach a second listener to the stream
-    getUriLinksStream().listen((Uri uri) {
-      print('got uri: ${uri?.path} ${uri?.queryParametersAll}');
-    }, onError: (err) {
-      print('got err: $err');
-    });
-
-    // Get the latest Uri
-    Uri initialUri;
-    String initialLink;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      initialUri = await getInitialUri();
-      print('initial uri: ${initialUri?.path}'
-          ' ${initialUri?.queryParametersAll}');
-      initialLink = initialUri?.toString();
-    } on PlatformException {
-      initialUri = null;
-      initialLink = 'Failed to get initial uri.';
-    } on FormatException {
-      initialUri = null;
-      initialLink = 'Bad parse the initial link as Uri.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _latestUri = initialUri;
-      _latestLink = initialLink;
+    _item = _items[widget.itemId];
+    _subscription = _item.onChanged.listen((Item item) {
+      if (!mounted) {
+        _subscription.cancel();
+      } else {
+        setState(() {
+          _item = item;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final queryParams = _latestUri?.queryParametersAll?.entries?.toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Item ${_item.itemId}"),
+      ),
+      body: Material(
+        child: Center(child: Text("Item status: ${_item.status}")),
+      ),
+    );
+  }
+}
 
-    return new MaterialApp(
-      home: new Scaffold(
-        key: _scaffoldKey,
-        appBar: new AppBar(
-          title: new Text('Plugin example app'),
-          bottom: new TabBar(
-            controller: _tabController,
-            tabs: <Widget>[
-              new Tab(text: 'STRING LINK'),
-              new Tab(text: 'URI'),
+class PushMessagingExample extends StatefulWidget {
+  @override
+  _PushMessagingExampleState createState() => _PushMessagingExampleState();
+}
+
+class _PushMessagingExampleState extends State<PushMessagingExample> {
+  String _homeScreenText = "Waiting for token...";
+  bool _topicButtonsDisabled = false;
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final TextEditingController _topicController =
+      TextEditingController(text: 'topic');
+
+  Widget _buildDialog(BuildContext context, Item item) {
+    return AlertDialog(
+      content: Text("Item ${item.itemId} has been updated"),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        FlatButton(
+          child: const Text('SHOW'),
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate == true) {
+        _navigateToItemDetail(message);
+      }
+    });
+  }
+
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    final Item item = _itemForMessage(message);
+    // Clear away dialogs
+    Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+    if (!item.route.isCurrent) {
+      Navigator.push(context, item.route);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+      });
+      print(_homeScreenText);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Push Messaging Demo'),
+        ),
+        // For testing -- simulate a message being received
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _showItemDialog(<String, dynamic>{
+                "id": "2",
+                "status": "out of stock",
+              }),
+          tooltip: 'Simulate Message',
+          child: const Icon(Icons.message),
+        ),
+        body: Material(
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Text(_homeScreenText),
+              ),
+              Row(children: <Widget>[
+                Expanded(
+                  child: TextField(
+                      controller: _topicController,
+                      onChanged: (String v) {
+                        setState(() {
+                          _topicButtonsDisabled = v.isEmpty;
+                        });
+                      }),
+                ),
+                FlatButton(
+                  child: const Text("subscribe"),
+                  onPressed: _topicButtonsDisabled
+                      ? null
+                      : () {
+                          _firebaseMessaging
+                              .subscribeToTopic(_topicController.text);
+                          _clearTopicText();
+                        },
+                ),
+                FlatButton(
+                  child: const Text("unsubscribe"),
+                  onPressed: _topicButtonsDisabled
+                      ? null
+                      : () {
+                          _firebaseMessaging
+                              .unsubscribeFromTopic(_topicController.text);
+                          _clearTopicText();
+                        },
+                ),
+              ])
             ],
           ),
-        ),
-        body: new ListView(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(8.0),
-          children: <Widget>[
-            new ListTile(
-              title: const Text('Link'),
-              subtitle: new Text('$_latestLink'),
-            ),
-            new ListTile(
-              title: const Text('Uri Path'),
-              subtitle: new Text('${_latestUri?.path}'),
-            ),
-            new ExpansionTile(
-              initiallyExpanded: true,
-              title: const Text('Query params'),
-              children: queryParams?.map((item) {
-                    return new ListTile(
-                      title: new Text('${item.key}'),
-                      trailing: new Text('${item.value?.join(', ')}'),
-                    );
-                  })?.toList() ??
-                  <Widget>[
-                    new ListTile(
-                      dense: true,
-                      title: const Text('null'),
-                    ),
-                  ],
-            ),
-            _cmdsCard(_cmds),
-          ],
-        ),
-      ),
-    );
+        ));
   }
 
-  Widget _cmdsCard(commands) {
-    Widget platformCmds;
-
-    if (commands == null) {
-      platformCmds = const Center(child: const Text('Unsupported platform'));
-    } else {
-      platformCmds = new Column(
-        children: <List<Widget>>[
-          [
-            const Text(
-                'To populate above fields open a terminal shell and run:\n')
-          ],
-          intersperse(
-              commands.map<Widget>((cmd) => new InkWell(
-                    onTap: () => _printAndCopy(cmd),
-                    child: new Text('\n$cmd\n', style: _cmdStyle),
-                  )),
-              const Text('or')),
-          [
-            new Text(
-                '(tap on any of the above commands to print it to'
-                ' the console/logger and copy to the device clipboard.)',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.caption),
-          ]
-        ].expand((el) => el).toList(),
-      );
-    }
-
-    return new Card(
-      margin: const EdgeInsets.only(top: 20.0),
-      child: new Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: platformCmds,
-      ),
-    );
-  }
-
-  _handleTabChange() {
-    if (_tabController.indexIsChanging) {
-      setState(() {
-        _type = UniLinksType.values[_tabController.index];
-      });
-      initPlatformState();
-    }
-  }
-
-  _printAndCopy(String cmd) async {
-    print(cmd);
-
-    await Clipboard.setData(new ClipboardData(text: cmd));
-    _scaffoldKey.currentState.showSnackBar(new SnackBar(
-      content: const Text('Copied to Clipboard'),
-    ));
+  void _clearTopicText() {
+    setState(() {
+      _topicController.text = "";
+      _topicButtonsDisabled = true;
+    });
   }
 }
 
-List<String> getCmds() {
-  String cmd;
-  String cmdSuffix = '';
-
-  if (Platform.isIOS) {
-    cmd = '/usr/bin/xcrun simctl openurl booted';
-    cmd = "";
-  } else if (Platform.isAndroid) {
-    cmd = '\$ANDROID_HOME/platform-tools/adb shell \'am start'
-        ' -a android.intent.action.VIEW'
-        ' -c android.intent.category.BROWSABLE -d';
-    cmd = "";    
-    cmdSuffix = "'";
-  } else {
-    return null;
-  }
-
-  // https://orchid-forgery.glitch.me/mobile/redirect/
-  return [
-    '$cmd "mqes://b-trust.bg/path/subpath"$cmdSuffix',
-    '$cmd "https://b-trust.bg/login/portion/?uid=123&token=abc"$cmdSuffix',
-    '$cmd "https://b-trust.bg/login/?arr%5b%5d=123&arr%5b%5d=abc'
-        '&addr=1%20Nowhere%20Rd&addr=Rand%20City%F0%9F%98%82"$cmdSuffix',
-  ];
-}
-
-List<Widget> intersperse(Iterable<Widget> list, Widget item) {
-  List<Widget> initialValue = [];
-  return list.fold(initialValue, (all, el) {
-    if (all.length != 0) all.add(item);
-    all.add(el);
-    return all;
-  });
+void main() {
+  runApp(
+    MaterialApp(
+      home: PushMessagingExample(),
+    ),
+  );
 }
